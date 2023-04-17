9e91e75b4bf924f8b9788c90a59e86b15b3d82a6
Aidan Williams <aidanwilliams01@gmail.com>
2023-04-17 15:48:05 -0700

    add functionality that returns all results in one window alert

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,9 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The addition of your numbers equals " + add(number1,number2));
-window.alert("The subtraction of your numbers equals " + subtract(number1, number2));
-window.alert("The multiplication of your numbers equals " + multiply(number1,number2));
-window.alert("The division of your numbers equals " + divide(number1, number2));
+const addition = "The addition of your numbers equals " + add(number1,number2)+ ".";
+const subtraction = "The subtraction of your numbers equals " + subtract(number1, number2) + ".";
+const multiplication = "The multiplication of your numbers equals " + multiply(number1,number2) + ".";
+const division = "The division of your numbers equals " + divide(number1, number2) + ".";
+
+window.alert(addition + " " + subtraction + " " + multiplication + " " + division);
