70948c89b66d4c985fea42a2312d3b9002983928
Aidan Williams <aidanwilliams01@gmail.com>
2023-04-17 15:54:17 -0700

    add functionality that returns the user's numbers along with the results

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,9 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-const addition = "The addition of your numbers equals " + add(number1,number2)+ ".";
-const subtraction = "The subtraction of your numbers equals " + subtract(number1, number2) + ".";
-const multiplication = "The multiplication of your numbers equals " + multiply(number1,number2) + ".";
-const division = "The division of your numbers equals " + divide(number1, number2) + ".";
+const addition = number1 + " + " + number2 + " = " + add(number1,number2)+ ".";
+const subtraction = number1 + " - " + number2 + " = " + subtract(number1, number2) + ".";
+const multiplication = number1 + " * " + number2 + " = " + multiply(number1,number2) + ".";
+const division = number1 + " / " + number2 + " = " + divide(number1, number2) + ".";
 
 window.alert(addition + " " + subtraction + " " + multiplication + " " + division);
